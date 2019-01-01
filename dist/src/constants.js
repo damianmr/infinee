98263b081f2298630a8ce72624f3483a06ca41fc
Damian M R <damianmr@users.noreply.github.com>
2019-01-01 22:45:04 +0900

    Removing ignored files

--- dist/src/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Language = {
-    EnglishUS: "en_US",
-};
-//# sourceMappingURL=constants.js.map
--- dist/src/logs.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=logs.js.map
