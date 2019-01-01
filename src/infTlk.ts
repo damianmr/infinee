bacefb4c5186f9a8e2cf2033b319128decf6e9ab
Damian M R <damianmr@users.noreply.github.com>
2019-01-01 22:40:41 +0900

    First passing tests regarding dialog.tlk file

--- src/infTlk.ts
+++ src/infTlk.ts
@@ -1,6 +1,7 @@
 // tslint:disable:no-console object-literal-sort-keys
 import { readFile as rF } from "fs";
 import { join } from "path";
+import { SmartBuffer } from "smart-buffer";
 import { promisify } from "util";
 import { LanguageCode } from "./constants";
 
@@ -8,19 +9,99 @@ const readFile = promisify(rF);
 
 const TLK_FILENAME = "dialog.tlk";
 
-function parseHeader(fileBuffer: Buffer): any {
-  
+interface IDialogEntry {
+  unknown: number;
+  soundName: string;
+  volumeVariance: number;
+  pitchVariance: number;
+  relativeOffset: number;
+  length: number;
 }
 
-export function read(installationPath: string, language: LanguageCode) {
-  const filePath: string = join(installationPath, language, TLK_FILENAME);
+interface IPopulatedDialogEntry extends IDialogEntry { text: string; }
 
-  console.log("Reading translations file: ", filePath);
-  readFile(filePath, null)
-  .then((contents) => {
-    console.log(contents);
-  })
-  .catch((err) => {
-    console.log("Error reading dialog.tlk file. ", err);
+interface IDialogsTable {
+  signature: string;
+  version: string;
+  unknown: string;
+  stringsCount: number;
+  stringsOffset: number;
+}
+
+interface IEmptyDialogsTable extends IDialogsTable { dialogs: IDialogEntry[]; }
+export interface IPopulatedDialogsTable extends IDialogsTable { dialogs: IPopulatedDialogEntry[]; }
+
+function buildDialogsTable(fileBuffer: Buffer): IEmptyDialogsTable {
+  const r = SmartBuffer.fromBuffer(fileBuffer);
+  const fileHeader = {
+    signature: r.readString(4),
+    version: r.readString(4),
+    unknown: r.readString(2),
+    stringsCount: r.readUInt32LE(),
+    stringsOffset: r.readUInt32LE(),
+  };
+
+  const dialogs = [];
+  // TODO Maybe iterate just once and compare performance results?
+  // I would be 'seeking' the file all the time
+  for (let i = 0; i < fileHeader.stringsCount; i++) {
+    dialogs[i] = {
+      unknown: r.readUInt16LE(),
+      soundName: r.readString(8),
+      volumeVariance: r.readUInt32LE(),
+      pitchVariance: r.readUInt32LE(),
+      relativeOffset: r.readUInt32LE(),
+      length: r.readUInt32LE(),
+      text: null,
+    };
+  }
+  return Object.assign({}, fileHeader, { dialogs });
+}
+
+function populateDialogsTable(
+  dialogsFileContents: Buffer,
+  dialogsIndex: IEmptyDialogsTable,
+): IPopulatedDialogsTable {
+  const r = SmartBuffer.fromBuffer(dialogsFileContents);
+  const dialogs: IPopulatedDialogEntry[] = [];
+  for (let i = 0; i < dialogsIndex.stringsCount; i++) {
+    r.readOffset = dialogsIndex.stringsOffset + dialogsIndex.dialogs[i].relativeOffset;
+    dialogs[i] = Object.assign({}, dialogsIndex.dialogs[i], {
+      text: r.readString(dialogsIndex.dialogs[i].length),
+    });
+  }
+
+  return Object.assign({}, dialogsIndex, { dialogs });
+}
+
+export function read(
+  installationPath: string,
+  language: LanguageCode,
+): Promise<IPopulatedDialogsTable> {
+  const filePath: string = join(installationPath, language, TLK_FILENAME);
+  // console.log("Reading translations file: ", filePath);
+  return new Promise((resolve, reject) => {
+    readFile(filePath, null).then((contents: Buffer) => {
+      const index: IPopulatedDialogsTable = populateDialogsTable(contents, buildDialogsTable(contents));
+      resolve(index);
+    })
+    .catch((err) => {
+      console.log("Error reading dialog.tlk file. ", err);
+      reject({
+        message: `Error reading dialog.tlk file.`,
+        installationPath,
+        language,
+        filePath,
+        originalError: err,
+      });
+    });
   });
+}
+
+export function getText(dialogsIndex: IPopulatedDialogsTable, index: number) {
+  if (index > dialogsIndex.dialogs.length || index < 0) {
+    throw new Error("Cannot retrieve text. Index out of bounds.");
+  }
+
+  return dialogsIndex.dialogs[index].text;
 }
--- test/infTlk.ts
+++ test/infTlk.ts
@@ -1,14 +1,56 @@
-import { expect } from "chai";
+import { fail } from "assert";
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
 import "mocha";
 import { Language } from "../src/constants";
-import { read } from "../src/infTlk";
+import { getText, IPopulatedDialogsTable, read } from "../src/infTlk";
 import { MOCK_INSTALL } from "./constants";
 
-describe("Read of translations file", () => {
+chai.use(chaiAsPromised);
 
-  it("should read the file properly", (done) => {
-    read(MOCK_INSTALL, Language.EnglishUS);
-    done();
+const KNOWN_STRINGS = [
+  { index: 0, value: "<NO TEXT>" },
+  { index: 1, value: "No, I'm sorry, none of them sound familiar." },
+  { index: 55003, value: "What is their true nature? What price are you talking about?" },
+  // tslint:disable-next-line:max-line-length
+  { index: 103240, value: "Only the most worthy and devoted servants of the Foehammer are granted these powerful holy symbols. \n\nSTATISTICS:\n\nEquipped abilities:\n– Strength: +1\n– Magic Resistance: +5%\n– Can memorize one extra 6th- and 7th-level Cleric spell\n\nWeight: 0" },
+];
+
+describe("Testing the proper reading of dialog.tlk file", () => {
+
+  it("should read the file properly", () => {
+    return read(MOCK_INSTALL, Language.EnglishUS).then((dialogsIndex: IPopulatedDialogsTable) => {
+      expect(dialogsIndex.signature).to.be.equal("TLK ");
+      expect(dialogsIndex.version).to.be.equal("V1  ");
+    });
   });
 
+  it("should've read the strings table properly (testing against a few known values)", async () => {
+    return read(MOCK_INSTALL, Language.EnglishUS).then((dialogsIndex: IPopulatedDialogsTable) => {
+      KNOWN_STRINGS.forEach(({ index, value }: { index: number, value: string }) => {
+        expect(dialogsIndex.dialogs[index].text).to.be.equal(value);
+      });
+    });
+  });
+
+});
+
+describe("Getting a text value from the dialogs file", () => {
+
+  let dialogsIndex: IPopulatedDialogsTable;
+
+  before(async () => {
+    dialogsIndex = await read(MOCK_INSTALL, Language.EnglishUS);
+  });
+
+  it("should retrieve a text from the index", () => {
+    expect(getText(dialogsIndex, KNOWN_STRINGS[0].index)).to.be.equal(KNOWN_STRINGS[0].value);
+    const lastKnownString = KNOWN_STRINGS[KNOWN_STRINGS.length - 1];
+    expect(getText(dialogsIndex, lastKnownString.index)).to.be.equal(lastKnownString.value);
+  });
+
+  it("should fail if trying to access an out of bounds index", () => {
+    expect(() => getText(dialogsIndex, 999000)).to.throw(/out of bounds/);
+    expect(() => getText(dialogsIndex, -1)).to.throw(/out of bounds/);
+  });
 });
